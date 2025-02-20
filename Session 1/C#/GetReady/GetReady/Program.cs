16d74ffbac47c37b0d712102980add2c4b5dc3f0
Sotir <sotirusama13@gmail.com>
2025-02-20 20:16:37 +0200

    Initialization of C# GetReady project using visual studio 2022

---
+++ Session 1/C#/GetReady/GetReady/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetReady
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
